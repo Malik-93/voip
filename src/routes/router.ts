469cdb3a04093217fe55ed07a39091859bca5c40
Mudassir Malik <Malikjrw147@gmail.com>
2022-11-18 17:12:55 +0500

    ...

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,7 +4,6 @@ import userRouter from './user.router';
 import enumRouter from './enum.router';
 import chatRouter from './chat.router';
 import issueRouter from './issue.router';
-import fileRouter from './file.router';
 const router: Router = express.Router();
 router.get("/", (_req: Request, _res: Response) => {
     return _res.end(`${process.env.PROJECT} server is up and running...`);
@@ -14,6 +13,5 @@ router.use('/api/v1/companies', companyRouter);
 router.use('/api/v1/users', userRouter);
 router.use('/api/v1/chats', chatRouter);
 router.use('/api/v1/issues', issueRouter);
-router.use('/api/v1/files', fileRouter);
 
 export = router;
