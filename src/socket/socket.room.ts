b97dde1efc9b52ad53b46b8e68d1b2491d9c8ce5
Mudassir Malik <Malikjrw147@gmail.com>
2022-12-09 19:42:20 +0500

    save chat when agent assigned

--- src/socket/socket.room.ts
+++ src/socket/socket.room.ts
@@ -5,12 +5,42 @@ import userRepository from '../repositories/user';
 import { IUser } from '../interfaces/user/user.interface';
 import request from 'request';
 const { INIT_NEW_CHAT, SEND_MESSAGE } = SOCKET_EVENTS;
+type Payload = {
+    chat_id: number | string,
+    message: {
+        message_type: number,
+        text: string
 
+    }
+}
 export default (io: any, socket: ISocket, namespace: string) => {
     const auth_token = socket.handshake.auth.auth_token;
     // console.log('__namespace__', namespace);
     // console.log('__socket.auth_user__', socket.auth_user);
     // socket.join(`${namespace}`)
+    const save_message = (payload: Payload) => {
+        const data = {
+            "chat_id": payload.chat_id,
+            "message": payload.message
+        }
+        request.post(
+            `${process.env.SERVER_URL}/api/v1/chats/create_update`,
+            {
+                json: data,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${auth_token}`
+                }
+            },
+            async function (error, response, body) {
+                // console.log('Response', response);
+                const all_agents = await userRepository.user_repo_find_many([{ "role.value": 2 }]) as IUser[];
+                if (all_agents.length)
+                    socket.to(`${all_agents.map(_agent => `${_agent.socket_id}`)}`).emit('new_chat_initiated', null);
+
+            }
+        );
+    }
     socket.on(`${INIT_NEW_CHAT}`, async (message_data: IMessage) => {
         const { _issue_id } = message_data;
         const payload = {
@@ -40,10 +70,18 @@ export default (io: any, socket: ISocket, namespace: string) => {
         );
     })
     socket.on(SEND_MESSAGE, async (message_data: IMessage) => {
+        console.log('message_data', message_data);
         const { _sender, _reciever, _chat_id } = message_data;
-        // console.log('_chat_id', _chat_id);
-        const reciever = <IUser>await userRepository.user_repo_find('_id', _reciever)
-        const sender = <IUser>await userRepository.user_repo_find('_id', _sender)
+        // @ts-ignore
+        const reciever = <IUser>await userRepository.user_repo_find('_id', _reciever?._id)
+        // @ts-ignore
+        const sender = <IUser>await userRepository.user_repo_find('_id', _sender?._id);
+        save_message({
+            chat_id: _chat_id, message: {
+                message_type: 1,
+                text: message_data.text as string
+            }
+        })
         socket.to(`${reciever.socket_id}`).emit('incoming_message', { ...message_data, _sender: sender, _reciever: reciever, _chat_id });
     })
 
